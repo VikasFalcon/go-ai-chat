6b4de402d5db11a35484674c9bdaed4d08d519fe
Vikas Sawant <vikassawant@Vikass-MacBook-Air.local>
2026-07-17 21:32:28 +0530

    retrieval logic implemented

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/VikasFalcon/go-ai-chat/internal/embed"
 	"github.com/VikasFalcon/go-ai-chat/internal/llm"
 	"github.com/VikasFalcon/go-ai-chat/internal/model"
+	"github.com/VikasFalcon/go-ai-chat/internal/retrieval"
 	"github.com/gin-gonic/gin"
 )
 
@@ -38,24 +38,47 @@ func ChatHandler(c *gin.Context) {
 }
 
 func EmbedHandler(c *gin.Context) {
-	var req model.EmbeddingReq
-
-	if err := c.ShouldBindJSON(&req); err != nil {
+	store := retrieval.Store{}
+	store.Add(retrieval.Document{
+		ID:        1,
+		Text:      "doc1",
+		Embedding: []float64{1, 2},
+	})
+	store.Add(retrieval.Document{
+		ID:        1,
+		Text:      "doc2",
+		Embedding: []float64{8, 9},
+	})
+	query := []float64{1.2, 2.1}
+	doc, err := store.Search(query)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 
-	log.Printf("Request prompt: %s", req.Text)
-	resp, err := embed.Generate(req)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	c.JSON(http.StatusOK, doc)
+	return
 
-	c.JSON(http.StatusOK, resp)
+	// var req model.EmbeddingReq
+
+	// if err := c.ShouldBindJSON(&req); err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{
+	// 		"error": err.Error(),
+	// 	})
+	// 	return
+	// }
+
+	// log.Printf("Request prompt: %s", req.Text)
+	// resp, err := embed.Generate(req)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{
+	// 		"error": err.Error(),
+	// 	})
+	// 	return
+	// }
+
+	// c.JSON(http.StatusOK, resp)
 
 }
